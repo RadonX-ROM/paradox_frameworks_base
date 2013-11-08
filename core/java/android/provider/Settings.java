d93c38d657dc14702b5cc434db016368522b1879
Lars Greiss <kufikugel@googlemail.com>
2013-11-08 22:59:42 +0100

    Framework: safe headset volume option (1 of 2)
    thanks DvTonder
    
    Change-Id: I5355820a24d48c1d8f54ea17a7022d891ad07857
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	media/java/android/media/AudioService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2740,6 +2740,12 @@ public final class Settings {
          */
         public static final String HOME_BUTTON_WAKE = "home_button_wake";
 
+	/**
+         * Whether to prevent loud volume levels when headset is first plugged in.
+         * @hide
+         */
+        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
