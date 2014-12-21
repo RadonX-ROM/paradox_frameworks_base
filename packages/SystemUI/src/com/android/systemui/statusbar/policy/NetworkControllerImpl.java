d95f81ae9359731a92401f93e7b026e112bebe69
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-21 23:38:10 +0700

    SystemUI: various fixes to data type display
    - Hide data type when Wi-Fi is in use (CAF bug)
    - Display "!" when data is disabled (CAF bug)
    - Display type in QS when Wi-Fi is connected to captive portal (AOSP bug)
    
    CAF bug was introduced in 228bccb9d9eaabf9b3884500e4351fa5091be5aa
    
    The proper logic is described at https://android.googlesource.com/platform/frameworks/base/+/23dd176
    
    Change-Id: I7a34077d604c66c357809c634fe64c570a16e260

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -762,8 +762,9 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 + " showAtLeastThreeGees=" + String.valueOf(mShowAtLeastThreeGees));
         }
 
+        int inetCondition = inetConditionForNetwork(ConnectivityManager.TYPE_MOBILE);
         TelephonyIcons.updateDataType(chosenNetworkType, mShowAtLeastThreeGees,
-            mShow4GforLTE, mHspaDataDistinguishable, mInetCondition);
+            mShow4GforLTE, mHspaDataDistinguishable, inetCondition);
     }
 
     protected void updateSimState(Intent intent) {
@@ -904,7 +905,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         }
     }
 
-    private int inetConditionForNetwork(int networkType) {
+    protected int inetConditionForNetwork(int networkType) {
         return (mInetCondition == 1 && mConnectedNetworkType == networkType) ? 1 : 0;
     }
 
@@ -1253,6 +1254,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         }
 
         // We want to update all the icons, all at once, for any condition change
+        updateIconSet();
         updateDataNetType();
         updateWimaxIcons();
         updateDataIcon();
