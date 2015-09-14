5544005cd34354a47dd119027003929162224bec
Joe Maples <frap129@gmail.com>
2015-09-14 19:56:45 -0400

    Revert "SystemUI: option to ignore the last app when clearing recents (1/2)"
    This reverts commit 36203a3252e0cab9a1440ad20c989310cefb4ff0.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3075,12 +3075,6 @@ public final class Settings {
         public static final String SIP_ALWAYS = "SIP_ALWAYS";
 
         /**
-         * Whether to dismiss all recents tasks
-         * @hide
-         */
-        public static final String RECENTS_CLEAR_ALL_DISMISS_ALL = "recents_clear_all_dismiss_all";
-
-        /**
          * One of the sip call options: Only if destination is a SIP address.
          * @hide
          */
