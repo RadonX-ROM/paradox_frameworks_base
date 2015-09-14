c0d75aa822863fe3f87f570b06d9353087457524
Joe Maples <frap129@gmail.com>
2015-09-14 19:56:56 -0400

    Revert "Framework: Add Fade in and Fade out effect to Recents FAB"
    This reverts commit 8d8319fa70cded2121de421dce9cd4f31601ec5e.

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -564,8 +564,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Dismiss Recents to the focused Task or Home
         dismissRecentsToFocusedTaskOrHome(true);
-
-        mRecentsView.endFABanimation();
     }
 
     /** Called when debug mode is triggered */
@@ -595,41 +593,27 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         }
     }
 
-    /** Called when the enter recents animation is triggered. */
-    public void onEnterAnimationTriggered() {
-        // Animate the SystemUI scrim views
-        mScrimViews.startEnterRecentsAnimation();
-        mRecentsView.startFABanimation();
-    }
-
     /**** RecentsView.RecentsViewCallbacks Implementation ****/
 
     @Override
     public void onExitToHomeAnimationTriggered() {
         // Animate the SystemUI scrim views out
         mScrimViews.startExitRecentsAnimation();
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onTaskViewClicked() {
-        // Mark recents as no longer visible
-        AlternateRecentsComponent.notifyVisibilityChanged(false);
-        mVisible = false;
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onTaskLaunchFailed() {
         // Return to Home
         dismissRecentsToHomeRaw(true);
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onAllTaskViewsDismissed() {
         mFinishLaunchHomeRunnable.run();
-        mRecentsView.endFABanimation();
     }
 
     @Override
